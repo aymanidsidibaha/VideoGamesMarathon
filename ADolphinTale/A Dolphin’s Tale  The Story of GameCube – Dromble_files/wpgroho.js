99c149ceb903c2e83f9a9ac2b2e78aaa2d89588d
Lunaqua <lunaqua@protonmail.com>
2019-06-13 09:23:56 +0100

    Actually Commiting the Changes...
    I made a mistake!

---
+++ ADolphinTale/A Dolphin’s Tale  The Story of GameCube – Dromble_files/wpgroho.js
@@ -0,0 +1,11 @@
+WPGroHo=jQuery.extend({my_hash:'',data:{},renderers:{},syncProfileData:function(hash,id){if(!WPGroHo.data[hash]){WPGroHo.data[hash]={};a=jQuery('div.grofile-hash-map-'+hash+' span').each(function(){WPGroHo.data[hash][this.className]=jQuery(this).text();});}
+WPGroHo.appendProfileData(WPGroHo.data[hash],hash,id);},appendProfileData:function(data,hash,id){for(var key in data){if(jQuery.isFunction(WPGroHo.renderers[key])){return WPGroHo.renderers[key](data[key],hash,id,key);}
+jQuery('#'+id).find('h4').after(jQuery('<p class="grav-extra '+key+'" />').html(data[key]));}}},WPGroHo);
+/*
+     FILE ARCHIVED ON 09:12:43 May 08, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:26:27 Dec 29, 2017.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
